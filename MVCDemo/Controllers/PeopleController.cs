edd5bbe0cc105e0743e135cd457a05b13a0639de
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-03 20:28:29 +0300

    Show List of Peoples model in ListPeople view

--- MVCDemo/Controllers/PeopleController.cs
+++ MVCDemo/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,17 @@ namespace MVCDemo.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Sarkawt", LastName = "Anyting", Age = 43 });
+            people.Add(new PersonModel { FirstName = "Savan", LastName = "Sarkawt", Age = 15 });
+            people.Add(new PersonModel { FirstName = "Balen", LastName = "Dlzar", Age = 23 });
+
+            return View(people);
         }
     }
 }
